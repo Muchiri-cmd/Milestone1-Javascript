a7f6ec73a43b09a9aee951de8cd1a10b8b436996
Muchiri-cmd <davismuchiri21@gmail.com>
2025-05-20 16:31:18 +0300

    Complete Challenge3:Perimeter of a Rectangle

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -8,6 +8,20 @@ function convert(minutes){
   return minutes * 60;
 }
 
+// Challenge 3:Find the perimeter of a rectangle
+function findPerimeter(length,width){
+  return 2 * (length + width);
+}
+
+// Challenge 4: Check Negative
+function checkNegative(num){
+  if(num < 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //Test cases
 console.log("Addition of two numbers");
 console.log(addition(3,5)); //8
@@ -16,3 +30,11 @@ console.log(addition(-6,9)); //3
 console.log("Convert minutes into seconds");
 console.log(convert(5)); //300
 console.log(convert(2)); //120
+
+console.log("Find the perimeter of a rectangle");
+console.log(findPerimeter(6,7)); //26
+console.log(findPerimeter(20,10)); //60
+
+console.log("Check Negative");
+console.log(checkNegative(-23)); //true
+console.log(checkNegative(55)); //false
