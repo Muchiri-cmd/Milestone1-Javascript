481a96ac2199197b9290acda3b15561df9641eff
Muchiri-cmd <davismuchiri21@gmail.com>
2025-05-20 16:27:36 +0300

    Complete Challenge2:Convert Minutes into Seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -3,7 +3,16 @@ function addition(num1,num2){
   return num1 + num2;
 }
 
+// Challenge 2: Convert minutes into seconds
+function convert(minutes){
+  return minutes * 60;
+}
+
 //Test cases
 console.log("Addition of two numbers");
 console.log(addition(3,5)); //8
 console.log(addition(-6,9)); //3
+
+console.log("Convert minutes into seconds");
+console.log(convert(5)); //300
+console.log(convert(2)); //120
