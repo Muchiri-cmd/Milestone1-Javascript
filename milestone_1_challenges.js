a16927e8a21d1b00ac09afb2e0ed13f3ed388d0c
Muchiri-cmd <davismuchiri21@gmail.com>
2025-05-20 16:40:04 +0300

    Complete Challenge 6: Largest Number

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -30,6 +30,17 @@ function canDrive(name,age){
   }
 }
 
+// Challenge 6: Largest Number
+function findLargest(a,b,c){
+  if(a > b && a > c){
+    return a;
+  } else if(b > a && b > c){
+    return b;
+  } else {
+    return c;
+  }
+}
+
 //Test cases
 console.log("Addition of two numbers");
 console.log(addition(3,5)); //8
@@ -50,3 +61,8 @@ console.log(isNegative(55)); //false
 console.log("Can I Drive");
 console.log(canDrive("Jane", 22)); //Jane is old enough to drive
 console.log(canDrive("June",12)); //June is not old enough to drive yet
+
+console.log("Largest Number");
+console.log(findLargest(5,9,3)); //9
+console.log(findLargest(10,10,10)); //10
+console.log(findLargest(-1,-5,-3)); //-1
