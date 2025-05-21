f81fdca2c8cdeeba055a448481b5be290bbbbb58
Muchiri-cmd <davismuchiri21@gmail.com>
2025-05-21 16:30:06 +0300

    Complete Challenge 2: Find Maximum value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,13 +1,28 @@
 //Challenge 1: Sum of Positives
-function sumofPositives(arr){
+function sumofPositives(arr) {
   let sum = 0;
-  for(let i = 0 ; i < arr.length; i++ ){
-    if(arr[i] > 0){ // Check if the number is positive
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) { // Check if the number is positive
       sum += arr[i];
     }
   }
   return sum;
 }
 
+// Challenge 2: Find maximum value
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
 //Test cases
-console.log(sumofPositives([1, -3, 5, -2, 9, -8 ])); //15
+console.log("Sum of Positives");
+console.log(sumofPositives([1, -3, 5, -2, 9, -8])); //15
+
+console.log("Find maximum value");
+console.log(findMax([3, 7, 2, 9, 5])); //returns 9
